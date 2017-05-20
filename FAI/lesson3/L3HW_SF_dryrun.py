dc695c109e251b5146785385d57192e0aba1d6f3
Wayne Nixalo <wayne.noxchi@gmail.com>
2017-05-20 01:21:20 -0400

    started working on L3HW State Farm

---
+++ FAI/lesson3/L3HW_SF_dryrun.py
@@ -0,0 +1,10 @@
+# This is something of a dryrun for the code in the JNB. Code here is run in
+# Atom using Hydrogen. Kernel: Python2.7
+
+# FAI1 - Practical Deep Learning I: Week 3: Convolutions
+# StateFarm Distracted Driver Kaggle Competition
+# Wayne Nixalo - 2017-May-20 00:48
+
+# Download the Data & get it into the right directories
+# kaggle-cli needs to be set up beforehand
+% kg download
